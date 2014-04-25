60b1c4c117c8aa02806908ad279c5a167e2ef6db
Johannes Link <business@johanneslink.net>
2014-04-25 16:20:38 +0200

    added kapitel-1 step 1

---
+++ scenarios/kapitel-1/steps/1/src/main/KeyCombinations.groovy
@@ -0,0 +1,8 @@
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class KeyCombinations {
+    KeyCombinations(List alphabet) {}
+
+    List<String> ofLength(int keyLength) {}
+}
---
+++ scenarios/kapitel-1/steps/1/src/test/KeyCombinationsTest.groovy
@@ -0,0 +1,12 @@
+import groovy.transform.CompileStatic
+import org.junit.Test
+
+@CompileStatic
+class KeyCombinationsTest {
+
+    @Test
+    void keysFromAlphabetOfLength1() {
+        def keyCombinations = new KeyCombinations(['a'])
+        assert keyCombinations.ofLength(1) == ['a']
+    }
+}
