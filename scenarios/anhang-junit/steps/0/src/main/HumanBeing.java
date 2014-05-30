24f55221c1633d13d1fef2ca9f5df7751fe355bd
Johannes Link <business@johanneslink.net>
2014-05-30 15:48:19 +0200

    anhang-junit step 0

---
+++ scenarios/anhang-junit/steps/0/src/main/HumanBeing.java
@@ -0,0 +1,35 @@
+public class HumanBeing {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public HumanBeing(String name) {
+        this.name = name;
+    }
+
+    public String computeFromName(DoWithName code) {
+        return code.withName(name);
+    }
+
+    public static void main(String[] args) {
+        HumanBeing son = new HumanBeing("Jannek");
+        DoWithName code = new DoWithName() {
+            @Override
+            public String withName(String name) {
+                return name + name;
+            }
+        };
+        System.out.println(son.computeFromName(code));
+    }
+
+    interface DoWithName {
+        String withName(String name);
+    }
+}
