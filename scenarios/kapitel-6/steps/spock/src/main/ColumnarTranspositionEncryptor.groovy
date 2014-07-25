d95fb6c0c6edf8565065163ef64861323c659f6a
Johannes Link <business@johanneslink.net>
2014-07-25 19:08:27 +0200

    working on columnar transposition with spock: Added gcontracts pre condition

--- scenarios/kapitel-6/steps/spock/src/main/ColumnarTranspositionEncryptor.groovy
+++ scenarios/kapitel-6/steps/spock/src/main/ColumnarTranspositionEncryptor.groovy
@@ -1,26 +1,32 @@
+import org.gcontracts.annotations.Requires
+
 class ColumnarTranspositionEncryptor {
 
     private Map<Integer, Integer> key
 
+    @Requires({
+        key.size() > 1
+        key.keySet() == (0..key.size() - 1).toSet()
+        key.values().toSet() == (0..key.size() - 1).toSet()
+    })
     ColumnarTranspositionEncryptor(Map<Integer, Integer> key) {
         this.key = key
     }
 
     String encrypt(String clearText) {
         def allChars = clearText.toList()
-        assert allChars == ['a', 'b', 'c', 'd', 'e', 'f']
         def blocks = splitInBlocks(allChars)
-        assert blocks == [['a', 'b'], ['c', 'd'], ['e', 'f']]
         blocks = createColumns(blocks)
-        assert blocks == [['a', 'c', 'e'], ['b', 'd', 'f']]
         blocks = orderByKey(blocks)
-        assert blocks == [['b', 'd', 'f'], ['a', 'c', 'e']]
 
         return blocks.flatten().join('')
     }
 
     private List orderByKey(List blocks) {
-        blocks.reverse()
+        if (key[0] == 1)
+            return blocks.reverse()
+        else
+            return blocks
     }
 
     private List createColumns(List<List<String>> blocks) {
@@ -31,12 +37,3 @@ class ColumnarTranspositionEncryptor {
         allChars.collate(key.size())
     }
 }
-
-/*
-        def allChars = clearText.toList()
-        def blocks = allChars.collate(2)
-        blocks = blocks.transpose()
-        blocks = blocks.reverse()
-        blocks.flatten().join('')
-
- */
--- scenarios/kapitel-6/steps/spock/src/test/ColumnarTranspositionEncryptionSpec.groovy
+++ scenarios/kapitel-6/steps/spock/src/test/ColumnarTranspositionEncryptionSpec.groovy
@@ -4,14 +4,25 @@ class ColumnarTranspositionEncryptionSpec extends Specification {
 
     ColumnarTranspositionEncryptor encryptor
 
-    def "encrypt text of length 6 with key of length 2"() {
-        encryptor = new ColumnarTranspositionEncryptor([1: 0, 0:1])
+    def "encrypt text of length 6 with permuted key of length 2"() {
+        encryptor = new ColumnarTranspositionEncryptor([1: 0, 0: 1])
 
         expect:
-            encryptor.encrypt(clearText) == cryptText
+        encryptor.encrypt(clearText) == cryptText
         where:
-            clearText   |   cryptText
-            'abcdef'    |   'bdface'
-            //'123456'    |   '246135'
+        clearText | cryptText
+        'abcdef'  | 'bdface'
+        '123456'  | '246135'
+    }
+
+    def "encrypt text of length 6 with unpermuted key of length 2"() {
+        encryptor = new ColumnarTranspositionEncryptor([0: 0, 1: 1])
+
+        expect:
+        encryptor.encrypt(clearText) == cryptText
+        where:
+        clearText | cryptText
+        'abcdef'  | 'acebdf'
+        '123456'  | '135246'
     }
 }
