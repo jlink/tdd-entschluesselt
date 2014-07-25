6502b04809dbddf1f06188c99bbe332bec787b02
Johannes Link <business@johanneslink.net>
2014-07-25 18:51:59 +0200

    working on columnar transposition with spock: Added inline asserts

--- scenarios/kapitel-6/steps/spock/src/main/ColumnarTranspositionEncryptor.groovy
+++ scenarios/kapitel-6/steps/spock/src/main/ColumnarTranspositionEncryptor.groovy
@@ -1,9 +1,42 @@
 class ColumnarTranspositionEncryptor {
 
+    private Map<Integer, Integer> key
+
     ColumnarTranspositionEncryptor(Map<Integer, Integer> key) {
+        this.key = key
     }
 
     String encrypt(String clearText) {
-        'bdface'
+        def allChars = clearText.toList()
+        assert allChars == ['a', 'b', 'c', 'd', 'e', 'f']
+        def blocks = splitInBlocks(allChars)
+        assert blocks == [['a', 'b'], ['c', 'd'], ['e', 'f']]
+        blocks = createColumns(blocks)
+        assert blocks == [['a', 'c', 'e'], ['b', 'd', 'f']]
+        blocks = orderByKey(blocks)
+        assert blocks == [['b', 'd', 'f'], ['a', 'c', 'e']]
+
+        return blocks.flatten().join('')
+    }
+
+    private List orderByKey(List blocks) {
+        blocks.reverse()
+    }
+
+    private List createColumns(List<List<String>> blocks) {
+        blocks.transpose()
+    }
+
+    private List<List<String>> splitInBlocks(List<String> allChars) {
+        allChars.collate(key.size())
     }
 }
+
+/*
+        def allChars = clearText.toList()
+        def blocks = allChars.collate(2)
+        blocks = blocks.transpose()
+        blocks = blocks.reverse()
+        blocks.flatten().join('')
+
+ */
--- scenarios/kapitel-6/steps/spock/src/test/ColumnarTranspositionEncryptionSpec.groovy
+++ scenarios/kapitel-6/steps/spock/src/test/ColumnarTranspositionEncryptionSpec.groovy
@@ -5,11 +5,13 @@ class ColumnarTranspositionEncryptionSpec extends Specification {
     ColumnarTranspositionEncryptor encryptor
 
     def "encrypt text of length 6 with key of length 2"() {
-        given:
-            encryptor = new ColumnarTranspositionEncryptor([1: 0, 0:1])
-        when:
-            def cryptText = encryptor.encrypt 'abcdef'
-        then:
-            cryptText == 'bdface'
+        encryptor = new ColumnarTranspositionEncryptor([1: 0, 0:1])
+
+        expect:
+            encryptor.encrypt(clearText) == cryptText
+        where:
+            clearText   |   cryptText
+            'abcdef'    |   'bdface'
+            //'123456'    |   '246135'
     }
 }
