b8dad03cf268a94d5ea21cb7e3b194e83632ecb7
Johannes Link <business@johanneslink.net>
2014-07-30 18:06:25 +0200

    kapitel-6:spock block splitting works

--- scenarios/kapitel-6/steps/spock/src/main/Blocks.groovy
+++ scenarios/kapitel-6/steps/spock/src/main/Blocks.groovy
@@ -1,6 +1,36 @@
 class Blocks {
     static List<List> splitInBlocks(List items, Map<Integer, Integer> key) {
         int lengthOfBlocks = Math.ceil(items.size() / key.size())
-        return items.collate(lengthOfBlocks)
+        List blocks = split(lengthOfBlocks, items, key)
+        fillBlocksWithNull(blocks, lengthOfBlocks)
+        return blocks
+    }
+
+    private static ArrayList split(int lengthOfBlocks, List items, Map<Integer, Integer> key) {
+        int missingItems = (lengthOfBlocks - items.size() % lengthOfBlocks) % lengthOfBlocks
+        def blocks = []
+        def currentBlock = []
+        for (int i = 0; i < items.size(); i++) {
+            if (i != 0 && atEndOfBlock(key[blocks.size() + 1] - 1, currentBlock.size(), lengthOfBlocks, key.size() - missingItems)) {
+                blocks.add(currentBlock)
+                currentBlock = []
+            }
+            currentBlock.add(items[i])
+        }
+        blocks.add(currentBlock)
+        blocks
+    }
+
+    private static void fillBlocksWithNull(List blocks, int lengthOfBlocks) {
+        blocks.findAll { it.size() < lengthOfBlocks }.each {
+            it.add(null)
+        }
+    }
+
+    private static boolean atEndOfBlock(int blockTargetIndex, int currentBlockIndex, int lengthOfBlocks, int fillInIndex) {
+        if (blockTargetIndex >= fillInIndex)
+            return currentBlockIndex % lengthOfBlocks == (lengthOfBlocks  - 1)
+        else
+            return currentBlockIndex % lengthOfBlocks == 0
     }
 }
--- scenarios/kapitel-6/steps/spock/src/test/BlocksSpec.groovy
+++ scenarios/kapitel-6/steps/spock/src/test/BlocksSpec.groovy
@@ -1,3 +1,4 @@
+import spock.lang.Ignore
 import spock.lang.Specification
 
 
@@ -11,5 +12,24 @@ class BlocksSpec extends Specification {
             def blocks = Blocks.splitInBlocks(items, key)
         then:
             blocks == [[1, 2, 3], [4, 5, 6]]
+    }
+
+
+    def splitCharsIntoBlocksThatAreNotDivisibleByKeySizeButNotPermuted() {
+        expect:
+            Blocks.splitInBlocks(items, key) == blocks
+        where:
+            items                   | key               | blocks
+            [1, 2, 3, 4, 5]         | [1:1, 2:2]        | [[1, 2, 3], [4, 5, null]]
+            [1, 2, 3, 4, 5, 6, 7]   | [1:1, 2:2, 3:3]   | [[1, 2, 3], [4, 5, null], [6, 7, null]]
+    }
+
+    def splitCharsIntoBlocksThatAreNotDivisibleByKeySizeAndPermuted() {
+        expect:
+            Blocks.splitInBlocks(items, key) == blocks
+        where:
+            items                   | key               | blocks
+            [1, 2, 3, 4, 5]         | [2:1, 1:2]        | [[1, 2, null], [3, 4, 5]]
+            [1, 2, 3, 4, 5, 6, 7]   | [1:2, 2:3, 3:1]   | [[1, 2, null], [3, 4, null], [5, 6, 7]]
     }
 }
