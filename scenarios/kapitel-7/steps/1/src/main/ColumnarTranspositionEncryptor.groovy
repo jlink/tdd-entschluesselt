456e6278204e9048f639eb3726ea07ca7b97bfb0
Johannes Link <business@johanneslink.net>
2015-01-19 17:38:49 +0100

    kapitel-7 step 1: Added class trunks to enable compilation of tests

---
+++ scenarios/kapitel-7/steps/1/src/main/ColumnarTranspositionEncryptor.groovy
@@ -0,0 +1,7 @@
+class ColumnarTranspositionEncryptor {
+    ColumnarTranspositionEncryptor(key) {
+    }
+    String encrypt(String s) {
+        null
+    }
+}
---
+++ scenarios/kapitel-7/steps/1/src/main/TranspositionKey.groovy
@@ -0,0 +1,3 @@
+class TranspositionKey {
+    static TranspositionKey anyOfLength(int i) {}
+}
