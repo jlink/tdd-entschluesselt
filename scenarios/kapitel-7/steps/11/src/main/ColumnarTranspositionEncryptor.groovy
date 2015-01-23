f7a3fdbe338e1f04fd92beea7059cee2ad76a8d5
Johannes Link <business@johanneslink.net>
2015-01-23 15:54:55 +0100

    kapitel-7 step 11

---
+++ scenarios/kapitel-7/steps/11/src/main/ColumnarTranspositionEncryptor.groovy
@@ -0,0 +1,25 @@
+import groovy.transform.CompileStatic
+import org.gcontracts.annotations.Requires
+
+@CompileStatic
+class ColumnarTranspositionEncryptor {
+
+    private final TranspositionKey key
+
+    @Requires({ key instanceof TranspositionKey })
+    ColumnarTranspositionEncryptor(TranspositionKey key) {
+        this.key = key
+    }
+
+    @Requires({ clearText.size() > 0 && hasOnlyValidChars(clearText) })
+    String encrypt(String clearText) {
+        List letters = clearText.toList()
+        List transposedRow = key.transpose(letters)
+        return transposedRow.join("")
+    }
+
+
+    private static boolean hasOnlyValidChars(String aString) {
+        return aString.find { !(it in 'a'..'z') } == null
+    }
+}
