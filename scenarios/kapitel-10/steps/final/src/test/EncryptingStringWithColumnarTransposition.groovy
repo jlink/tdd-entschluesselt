9aaacd10f82f0f3a8af2a84404d16798b71ef272
Johannes Link <business@johanneslink.net>
2015-01-28 16:19:47 +0100

    kapitel-10 steps 8 and final

---
+++ scenarios/kapitel-10/steps/final/src/test/EncryptingStringWithColumnarTransposition.groovy
@@ -0,0 +1,41 @@
+import groovy.transform.CompileStatic
+import org.junit.Test
+
+@CompileStatic
+class EncryptingStringWithColumnarTransposition {
+    @Test
+    void stringOfLength1RemainsUnchanged() {
+        def anyKey = new TranspositionKey(0:0)
+        ColumnarTranspositionEncryptor encryptor = new ColumnarTranspositionEncryptor(anyKey)
+        assert encryptor.encrypt("a") == "a"
+        assert encryptor.encrypt("b") == "b"
+    }
+
+    @Test
+    void simpleTranspositionSwapsTwoLetters() {
+        def swappingKey = new TranspositionKey(0:1, 1:0)
+        ColumnarTranspositionEncryptor encryptor = new ColumnarTranspositionEncryptor(swappingKey)
+        assert encryptor.encrypt("ab") == "ba"
+        assert encryptor.encrypt("xy") == "yx"
+    }
+
+    @Test
+    void transpose3Letters() {
+        def key = new TranspositionKey(0:1, 1:2, 2:0)
+        assert key.transpose(['a', 'b', 'c']) == ['c', 'a', 'b']
+    }
+
+    @Test
+    void transposeWithSomeLettersOnSamePosition() {
+        def key = new TranspositionKey(0:3, 1:1, 2:0, 3:2)
+        assert key.transpose("qrst".toList()) == "srtq".toList()
+    }
+
+    @Test
+    void columnarTranspositionWithNTimesTheKeyLength() {
+        def nonSwappingKey = new TranspositionKey(0:0, 1:1, 2:2)
+        ColumnarTranspositionEncryptor encryptor = new ColumnarTranspositionEncryptor(nonSwappingKey)
+        assert encryptor.encrypt("abcxyz") == "axbycz"
+        assert encryptor.encrypt("abcdefghi") == "adgbehcfi"
+    }
+}
