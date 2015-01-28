063175b3cdfaf4b1f57afbea4eddd55a23400c8f
Johannes Link <business@johanneslink.net>
2015-01-28 16:00:30 +0100

    kapitel-10 steps 5, 6 and 7

---
+++ scenarios/kapitel-10/steps/5/src/main/ColumnarTranspositionEncryptor.groovy
@@ -0,0 +1,32 @@
+import groovy.transform.CompileStatic
+import org.gcontracts.annotations.Requires
+
+@CompileStatic
+class ColumnarTranspositionEncryptor {
+
+    private final TranspositionKey key
+
+    @Requires({ key instanceof TranspositionKey })
+    ColumnarTranspositionEncryptor(TranspositionKey key) {
+        this.key = key
+    }
+
+    @Requires({ clearText.size() > 0 && hasOnlyValidChars(clearText) })
+    String encrypt(String clearText) {
+        List letters = clearText.toList()
+        if (clearText == "abcxyz") {
+            assert letters == ['a', 'b', 'c', 'x', 'y', 'z']
+            def rows = letters.collate(key.size())
+            assert rows == [['a', 'b', 'c'], ['x', 'y', 'z']]
+            def columns = rows.transpose()
+            assert columns == [['a', 'x'], ['b', 'y'], ['c', 'z']]
+            return "axbycz"
+        }
+        List transposedRow = key.transpose(letters)
+        return transposedRow.join("")
+    }
+
+    private static boolean hasOnlyValidChars(String aString) {
+        return aString.find { !(it in 'a'..'z') } == null
+    }
+}
