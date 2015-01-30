dee8bba6ee9b2e9528fc3a0f66d5944fec56daac
Johannes Link <business@johanneslink.net>
2015-01-30 14:16:59 +0100

    kapitel-12 steps 4

---
+++ scenarios/kapitel-12/steps/4/src/test/EncryptingStringWithColumnarTransposition.groovy
@@ -0,0 +1,81 @@
+import groovy.transform.CompileStatic
+import org.junit.Test
+
+@CompileStatic
+class EncryptingStringWithColumnarTransposition {
+    @Test
+    void stringOfLength1RemainsUnchanged() {
+        def anyKey = new TranspositionKey(0: 0)
+        ColumnarTranspositionEncryptor encryptor =
+                new ColumnarTranspositionEncryptor(anyKey)
+        assert encryptor.encrypt("a") == "a"
+        assert encryptor.encrypt("b") == "b"
+    }
+
+    @Test
+    void simpleTranspositionSwapsTwoLetters() {
+        def swappingKey = new TranspositionKey(0: 1, 1: 0)
+        ColumnarTranspositionEncryptor encryptor =
+                new ColumnarTranspositionEncryptor(swappingKey)
+        assert encryptor.encrypt("ab") == "ba"
+        assert encryptor.encrypt("xy") == "yx"
+    }
+
+    @Test
+    void transpose3Letters() {
+        def key = new TranspositionKey(0: 1, 1: 2, 2: 0)
+        assert key.transpose(['a', 'b', 'c']) == ['c', 'a', 'b']
+    }
+
+    @Test
+    void transposeWithSomeLettersOnSamePosition() {
+        def key = new TranspositionKey(0: 3, 1: 1, 2: 0, 3: 2)
+        assert key.transpose("qrst".toList()) == "srtq".toList()
+    }
+
+    @Test
+    void transpositionKeyCanTransposeAnyList() {
+        def key = new TranspositionKey(0: 1, 1: 2, 2: 0)
+        assert key.transpose([1, 2, 3]) == [3, 1, 2]
+    }
+
+    @Test
+    void transpositionKeyCanHandleListsShorterThanKey() {
+        def key = new TranspositionKey(0: 1, 1: 5, 2: 4, 3: 3, 4: 0, 5: 2)
+        assert key.transpose([0, 1, 2, 3]) == [0, 3, 2, 1]
+    }
+
+    @Test
+    void multirowTranspositionWithTrivialKey() {
+        def nonSwappingKey = new TranspositionKey(0: 0, 1: 1, 2: 2)
+        ColumnarTranspositionEncryptor encryptor =
+                new ColumnarTranspositionEncryptor(nonSwappingKey)
+        assert encryptor.encrypt("abcxyz") == "axbycz"
+        assert encryptor.encrypt("abcdefghi") == "adgbehcfi"
+    }
+
+    @Test
+    void multirowTranspositionWithNonTrivialKey() {
+        def key = new TranspositionKey(0: 1, 1: 2, 2: 0)
+        ColumnarTranspositionEncryptor encryptor =
+                new ColumnarTranspositionEncryptor(key)
+        assert encryptor.encrypt("abcdefghi") == "cfiadgbeh"
+    }
+
+    //@Test
+    void longerTextWithLongerKey() {
+        def key = new TranspositionKey(0: 4, 1: 1, 2: 2, 3: 3, 4: 0, 5: 5)
+        ColumnarTranspositionEncryptor encryptor =
+                new ColumnarTranspositionEncryptor(key)
+        assert encryptor.encrypt("dieschlachtbeginntbeimorgengrauen") ==
+                "ctnoriageeeeciinnshnmgdlebguhbtra"
+    }
+
+    @Test
+    void multirowTranspositionWithNonFittingClearText() {
+        def key = new TranspositionKey(0: 1, 1: 2, 2: 0)
+        ColumnarTranspositionEncryptor encryptor =
+                new ColumnarTranspositionEncryptor(key)
+        assert encryptor.encrypt("abcd") == "cadb"
+    }
+}
