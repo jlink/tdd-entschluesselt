579ab73f5ee0e901133e38ebec76cbbc2af7c743
Johannes Link <business@johanneslink.net>
2015-01-30 12:51:04 +0100

    kapitel-12 steps 1, 2

---
+++ scenarios/kapitel-12/steps/1/src/main/ColumnarTranspositionEncryptor.groovy
@@ -0,0 +1,39 @@
+import groovy.transform.CompileStatic
+import org.gcontracts.annotations.Requires
+
+@CompileStatic
+class ColumnarTranspositionEncryptor {
+
+    private final TranspositionKey key
+
+    @Requires({ key instanceof TranspositionKey })
+    ColumnarTranspositionEncryptor(TranspositionKey key) {
+        this.key = key
+    }
+
+    @Requires({ clearText.size() > 0 && hasOnlyValidChars(clearText) })
+    String encrypt(String clearText) {
+        List letters = clearText.toList()
+        def rows = partitionByKeySize(letters)
+        def columns = rows2Columns(rows)
+        columns = key.transpose(columns)
+        def cryptedLetters = flattenNestedLists(columns)
+        return cryptedLetters.join("")
+    }
+
+    private List<List<String>> partitionByKeySize(List letters) {
+        letters.collate(key.size())
+    }
+
+    private List rows2Columns(List<List> rows) {
+        rows.transpose()
+    }
+
+    private List flattenNestedLists(List<List> columns) {
+        (List) columns.flatten()
+    }
+
+    private static boolean hasOnlyValidChars(String aString) {
+        return aString.find { !(it in 'a'..'z') } == null
+    }
+}
